51130fee5fff7222b46772d2d42e23be9ef26757
Vinoth kumar <vinothpkumar333@gmail.com>
2020-01-24 16:23:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from colorama import Fore
+fh=open("URL.txt","r")
+websites=[]
+for each in fh.readlines():
+ websites.append(each.strip("\n"))
+print websites
+for i in websites:
+ try:
+ r=requests.get("http://"+i,timeout=5,verify=False)
+ print (Fore.GREEN+"Alive Endpoint:"+" "+i)
+ except:
+ print (Fore.RED+"Something went wrong:"+" "+i)
+fh.close()
+print "<----Scan completed---->
